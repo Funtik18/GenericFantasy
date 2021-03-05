7e60048900321a23e596ae6b06de9e0f4ca2420a
hazker <zov.zon@gmail.com>
2021-03-05 18:51:04 +0700

    MovementTheGreatBeginning

---
+++ Assets/Scripts/TacticFight/MenuScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+public class MenuScript 
+{
+    [MenuItem("Tools/Map/Assign Tile Material")]
+    public static void AssignTileMaterial()
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+        Material material = Resources.Load<Material>("Materials/Tile");
+        foreach (var item in tiles)
+        {
+            item.GetComponent<Renderer>().material = material;
+        }
+    }
+    [MenuItem("Tools/Map/Assign Tile Script")]
+    public static void AssignTileScript()
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+        foreach (var item in tiles)
+        {
+            item.AddComponent<Tile>();
+        }
+    }
+}
---
+++ Assets/Scripts/TacticFight/PlayerMov.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : TacticMove
+{
+    private Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!moving)
+        {
+            FindSelectableTiles();
+            CheckClick();
+        }
+        else
+        {
+
+        }
+
+    }
+
+    void CheckClick()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Tile")
+                {
+                    Tile t = hit.collider.GetComponent<Tile>();
+                    if (t.selectable)
+                    {
+                        t.target = true;
+                        moving = true;
+                    }
+                }
+            }
+        }
+    }
+}
