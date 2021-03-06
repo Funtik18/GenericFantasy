ce204ce82d98e6f4eda4103e68f6a8a9b5f42b06
Igor Davidovich <sir.funtik18@gmail.com>
2021-03-06 21:31:19 +0700

    add pngs

---
+++ Assets/Scripts/Dices/Dice.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Dice
+{
+	public readonly DiceType diceType = DiceType.Cube;
+	public int MaxDiceValue
+	{
+		get => (int)diceType;
+	}
+
+	//success rolls, reaction rolls и damage rolls
+	public Dice(DiceType diceType)
+	{
+		this.diceType = diceType;
+	}
+
+	public void Roll()
+	{
+		Debug.LogError(Random.Range(0, MaxDiceValue));
+	}
+
+	public void RollSuccess()
+	{
+
+	}
+}
+public enum DiceType : int
+{
+	Tetrahedron = 4,
+	Cube = 6,
+	Octahedron = 8,
+	Dodecahedron = 12,
+	Icosahedron = 20,
+}
---
+++ Assets/Scripts/Dices/DiceAdapter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceAdapter : MonoBehaviour
+{
+
+	private void Awake()
+	{
+	}
+	public void Roll()
+	{
+
+	}   
+}
