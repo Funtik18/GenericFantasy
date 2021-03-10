c069ebed30435bc2631b1f3c5ee0e9713b4e2e98
Davidovich Igor Alexandrovich <sir.funtik18@gmail.com>
2021-03-10 19:22:57 +0700

    fix merge

--- Assets/Scripts/TacticFight/NPCMove.cs
+++ Assets/Scripts/TacticFight/NPCMove.cs
@@ -1,74 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class NPCMove : TacticMove
-{
-    GameObject target;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Init();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.DrawRay(transform.position, transform.forward);
-        if (!turn)
-        {
-            return;
-        }
-        if (!moving)
-        {
-            FindNearestTarget();
-            CalculatePath();
-            FindSelectableTiles();
-
-            actualTargetTile.target = true;
-        }
-        else
-        {
-            Move();
-        }
-        if (myAttack.CheckAttack())
-        {
-            myAttack.Attack();
-        }
-    }
-
-    void CalculatePath()
-    {
-        Tile targetTile = GetTargetTile(target);
-
-        FindPath(targetTile);
-    }
-
-    void FindNearestTarget()
-    {
-        GameObject[] targets = GameObject.FindGameObjectsWithTag("Player");
-
-        GameObject nearest = null;
-        float distance = Mathf.Infinity;
-
-        foreach (GameObject item in targets)
-        {
-            float d = Vector3.Distance(transform.position, item.transform.position);
-
-            if (d < distance)
-            {
-                distance = d;
-                nearest = item;
-            }
-        }
-
-        target = nearest;
-    }
-}
-=======
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -141,4 +71,3 @@ public class NPCMove : TacticMove
 
     }
 }
->>>>>>> Develop
--- Assets/Scripts/TacticFight/PlayerMov.cs
+++ Assets/Scripts/TacticFight/PlayerMov.cs
@@ -1,64 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerMov : TacticMove
-{
-    private Camera cam;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Init();
-        cam = Camera.main;
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        Debug.DrawRay(transform.position, transform.forward);
-        if (!turn)
-        {
-            return;
-        }
-        if (!moving)
-        {
-            FindSelectableTiles();
-            CheckClick();
-        }
-        else
-        {
-            TurnManager.Instance.ClearCal();
-            Move();        
-        }
-        myAttack.CheckAttack();
-    }
-
-    void CheckClick()
-    {
-        if (Input.GetMouseButtonUp(0))
-        {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (hit.collider.tag == "Tile")
-                {
-                    Tile t = hit.collider.GetComponent<Tile>();
-                   
-                    if (t.selectable)
-                    {
-                        MoveToTile(t);
-                    }
-                }
-            }
-        }
-    }
-}
-=======
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -129,4 +69,3 @@ public class PlayerMov : TacticMove
     }
 
 }
->>>>>>> Develop
