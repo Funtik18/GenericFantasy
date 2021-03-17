3191bf37717d1fa6b02f69a9d393c1801c877dc2
Davidovich Igor Alexandrovich <sir.funtik18@gmail.com>
2021-03-17 12:52:27 +0700

    upd

---
+++ Assets/Scripts/Customization/PlayerChief.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine.Events;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Sirenix.OdinInspector;
+
+public class PlayerChief : MonoBehaviour
+{
+    public QuartetUI quartetUI;
+    public Quartet quartet;
+
+	private void Awake()
+	{
+		quartetUI.Initialization(quartet);
+	}
+
+
+	[Button]
+	private void Save()
+	{
+		SaveLoaderManager.SavePlayerQuartet(quartet);//сохраняем
+	}
+
+	[Button]
+	private void ClearSaves()
+	{
+		SaveLoaderManager.IsFirstTime = true;
+		SaveLoaderManager.DestroyDirectory();
+	}
+}
